c9bae3bd3db94f482fa2452c9af6bc333db6e7c4
My Name <srivastavaadi129@gmail.com>
2023-12-28 19:50:12 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ToDo from "./component/ToDo";
+
+
+const App = () =>{
+    return(
+        <>
+            
+            <ToDo/>
+        </>
+    )
+}
+
+export default App;
